92a5206ed9be76f33cdde26268fe496a3d16f55d
Edgar <edgar.mata.1911@gmail.com>
2020-05-15 16:29:55 -0600

    Threads
    Threads agregados en el main

--- ADONET/Program.cs
+++ ADONET/Program.cs
@@ -9,7 +9,29 @@ namespace ADONET
         [STAThread]
         public static void Main(String[] args)
         {
-            DataAccess.getInstance("Pooling").execQuery("Select * from Customer");
+            createThreads(50,"Pooling","Select * from Customer");
+        }
+		public void createThreads(int pQuantity,string pTypeConnection,string pCommand)
+        {
+            Thread[] threads = new Thread[pQuantity];
+            for(int threadIndex = 0; threadIndex<pQuantity; threadIndex++)
+            {
+                threads[threadIndex] = new Thread(new ParameterizedThreadStart(abrirConexion));
+            }
+            for (int threadIndex = 0; threadIndex < pQuantity; threadIndex++)
+            {
+                String[] parametters = {pTypeConnection,pCommand};
+                threads[threadIndex].Start(parametters);
+            }
+            for (int threadIndex = 0; threadIndex < pQuantity; threadIndex++)
+            {
+                threads[threadIndex].Join();
+            }
+        }
+        public void abrirConexion(object pParametters)
+        {
+            String[] parametters = (String[])pParametters;
+            DataAccess.getInstance(parametters[0]).execQuery(parametters[1]);
         }
     }
 }
